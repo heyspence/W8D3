3aacb17d53bde583be89abdb19a991c2f023d422
“Spencer <Spencer@bookservo.com>
2023-10-11 14:40:59 -0400

    finish clock

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/callbacks.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/callbacks.js
@@ -4,31 +4,37 @@ class Clock {
     // 2. Store the hours, minutes, and seconds.
     // 3. Call printTime.
     // 4. Schedule the tick at 1 second intervals.
-    this.date = new Date()
-    this.hrs = this.date.getHours()
-    this.min = this.date.getMinutes()
-    this.sec = this.date.getSeconds()
-    this.printTime()
-    setInterval(_tick.bind(this.date), 1000)
-    debugger
-  }
+    this.date = new Date();
+    this.hrs = this.date.getHours();
+    this.min = this.date.getMinutes();
+    this.sec = this.date.getSeconds();
+    this.printTime();
+    setInterval(this._tick.bind(this), 1000);
+  };
 
   printTime() {
     // Format the time in HH:MM:SS
     // Use console.log to print it.
-    console.log(`${this.hrs}:${this.min}:${this.sec}`)
-  }
+    console.log(`${this.hrs}:${this.min}:${this.sec}`);
+  };
 
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
-    return sec + 1
-    this.printTime()
-    if (this.sec > 60) {
+    this.sec += 1
+    this.printTime();
+    if (this.sec === 59) {
         this.sec = 0
-        this.min += 1
+        this.min++
+    }
+    
+    if (this.min === 59){
+      this.min = 0
+      this.hrs++
     }
   }
-}
+};
 
 const clock = new Clock()
+
+// console.log(clock)
